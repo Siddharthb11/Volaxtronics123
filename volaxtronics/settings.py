7cad30c5f93ca5666b2048f6a84ee478281797d4
Siddharthb11 <130811659+Siddharthb11@users.noreply.github.com>
2024-09-22 20:06:36 +0530

    Update settings.py

--- volaxtronics/settings.py
+++ volaxtronics/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--bn4mcs%5a3*)6hg0*&%4k_z-c=_^6olm=9)$-z6sf*c3*ho=v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.110.101.109']
+ALLOWED_HOSTS = ['3.109.216.215']
 
 
 # Application definition
